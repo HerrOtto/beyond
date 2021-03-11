b000fe6e72fba11bb0e9be6e71c8681793c2e22c
Saxen <info@netzmal.de>
2021-03-11 23:47:43 +0100

    Prepare update system

--- update.php
+++ update.php
@@ -12,6 +12,25 @@ if (!$tools->checkRole('admin,view')) {
 <html>
 <head>
     <title>Update</title>
+
+    <style>
+        .status {
+            font-weight: bold;
+            margin-bottom: 12px;
+        }
+
+        .note {
+            font-weight: lighter;
+            margin-bottom: 12px;
+        }
+
+        .error {
+            font-weight: lighter;
+            margin-bottom: 12px;
+            color: #ff0000;
+        }
+    </style>
+
     <?php require_once __DIR__ . '/inc/head.php'; ?>
 </head>
 <body class="sb-nav-fixed">
@@ -27,42 +46,48 @@ if (!$tools->checkRole('admin,view')) {
                 <ol class="breadcrumb mb-4 mt-4">
                     <li class="breadcrumb-item active">Update</li>
                 </ol>
-                <?php
-                try {
 
-                    // Check current version on github
-                    print "<div class='status'>Checking current version</div>";
-                    $githubUrl = 'https://raw.githubusercontent.com/HerrOtto/beyond/master/version.json?nocache=' . urlencode(microtime(true) . bin2hex(random_bytes(10)));
-                    print '<div class="note">URL: ' . $githubUrl . '</div>';
-                    $versionFromGithub = $tools->httpGet($githubUrl);
-                    if ($versionFromGithub == '') {
-                        throw new Exception('Cannot retrieve version information from GitHub [https://raw.githubusercontent.com/HerrOtto/beyond/master/version.json]');
-                    }
-                    print '<div class="note">Result: ' . $versionFromGithub . '</div>';
-                    $versionFromGithubJson = json_decode($versionFromGithub);
-                    print '<div class="note">Version on GitHub: ' . $versionFromGithubJson->version . '</div>';
+                <div class="box m-4 mt-5">
+                    <?php
+                    try {
 
-                    // Check current installed version
-                    print "<div class='status'>Checking installed version</div>";
-                    if (!file_exists(__DIR__ . '/version.json')) {
-                        $currentVersion = -1;
-                    } else {
-                        $currentVersion = trim(file_get_contents(__DIR__ . '/version.json'));
-                        if ($currentVersion == '') {
-                            throw new Exception('Cannot retrieve current version information');
+                        // Check current version on github
+                        print "<div class='status'>Checking current version</div>";
+                        $githubUrl = 'https://raw.githubusercontent.com/HerrOtto/beyond/master/version.json?nocache=' . urlencode(microtime(true) . bin2hex(random_bytes(10)));
+                        print '<div class="note">URL: ' . $githubUrl . '</div>';
+                        $versionFromGithub = $tools->httpGet($githubUrl);
+                        if ($versionFromGithub == '') {
+                            throw new Exception('Cannot retrieve version information from GitHub [https://raw.githubusercontent.com/HerrOtto/beyond/master/version.json]');
                         }
-                        $currentVersionJson = json_decode($currentVersion);
-                        $currentVersion = $currentVersionJson->version;
-                    }
-                    print '<div class="note">Installed version: ' . $currentVersion . '</div>';
-                    if ($currentVersion >= $versionFromGithubJson->version) {
-                        throw new Exception('Current version [' . $currentVersion . '] is newer or equal to Github version [' . $versionFromGithubJson->version . ']');
-                    }
+                        print '<div class="note">Result: ' . $versionFromGithub . '</div>';
+                        $versionFromGithubJson = json_decode($versionFromGithub);
+                        print '<div class="note">Version on GitHub: ' . $versionFromGithubJson->version . '</div>';
 
-                } catch (Exception $e) {
-                    print '<div class="error">' . $e->getMessage() . '</div>';
-                }
-                ?>
+                        // Check current installed version
+                        print "<div class='status'>Checking installed version</div>";
+                        if (!file_exists(__DIR__ . '/version.json')) {
+                            $currentVersion = -1;
+                        } else {
+                            $currentVersion = trim(file_get_contents(__DIR__ . '/version.json'));
+                            if ($currentVersion == '') {
+                                throw new Exception('Cannot retrieve current version information');
+                            }
+                            $currentVersionJson = json_decode($currentVersion);
+                            $currentVersion = $currentVersionJson->version;
+                        }
+                        print '<div class="note">Installed version: ' . $currentVersion . '</div>';
+                        if ($currentVersion >= $versionFromGithubJson->version) {
+                            throw new Exception('Current version [' . $currentVersion . '] is newer or equal to Github version [' . $versionFromGithubJson->version . ']');
+                        }
+
+                        print '<div class="note">Update ready</div>';
+                        print '<button onclick="location.href=\'' . $config->get('base', 'server.baseUrl') . '/updateExecure.php\';">Install update now!</button>';
+
+                    } catch (Exception $e) {
+                        print '<div class="error">' . $e->getMessage() . '</div>';
+                    }
+                    ?>
+                </div>
 
                 <?php require_once __DIR__ . '/inc/endSite.php'; ?>
             </div>
