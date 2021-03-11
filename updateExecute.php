3d62ca8d0e3614bd2fdcdd5e291c28ce46851402
Saxen <info@netzmal.de>
2021-03-11 23:38:28 +0100

    Prepare update system

--- updateExecute.php
+++ updateExecute.php
@@ -12,6 +12,25 @@ if (!$tools->checkRole('admin,view')) {
 <html>
 <head>
     <title>Update</title>
+
+    <style>
+        .status {
+            font-weight: bold;
+            margin-bottom: 12px;
+        }
+
+        .note {
+            font-weight: lighter;
+            margin-bottom: 12px;
+        }
+
+        .error {
+            font-weight: lighter;
+            margin-bottom: 12px;
+            color: #ff0000;
+        }
+    </style>
+
     <?php require_once __DIR__ . '/inc/head.php'; ?>
 </head>
 <body class="sb-nav-fixed">
@@ -28,22 +47,27 @@ if (!$tools->checkRole('admin,view')) {
                     <li class="breadcrumb-item active">Update</li>
                 </ol>
 
-                <div class="box mb-4">
+                <div class="box m-4 mt-5">
 
                     <?php
                     try {
 
                         // Check current version on github
+                        print "<div class='status'>Checking current version</div>";
                         $githubUrl = 'https://raw.githubusercontent.com/HerrOtto/beyond/master/version.json?nocache=' . urlencode(microtime(true) . bin2hex(random_bytes(10)));
+                        print '<div class="note">URL: ' . $githubUrl . '</div>';
                         $versionFromGithub = $tools->httpGet($githubUrl);
                         if ($versionFromGithub == '') {
-                            throw new Exception('Cannot retrieve version information from GitHub');
+                            throw new Exception('Cannot retrieve version information from GitHub [https://raw.githubusercontent.com/HerrOtto/beyond/master/version.json]');
                         }
+                        print '<div class="note">Result: ' . $versionFromGithub . '</div>';
                         $versionFromGithubJson = json_decode($versionFromGithub);
+                        print '<div class="note">Version on GitHub: ' . $versionFromGithubJson->version . '</div>';
 
                         // Check current installed version
+                        print "<div class='status'>Checking installed version</div>";
                         if (!file_exists(__DIR__ . '/version.json')) {
-                            $currentVersion = 1;
+                            $currentVersion = -1;
                         } else {
                             $currentVersion = trim(file_get_contents(__DIR__ . '/version.json'));
                             if ($currentVersion == '') {
@@ -52,18 +76,58 @@ if (!$tools->checkRole('admin,view')) {
                             $currentVersionJson = json_decode($currentVersion);
                             $currentVersion = $currentVersionJson->version;
                         }
+                        print '<div class="note">Installed version: ' . $currentVersion . '</div>';
                         if ($currentVersion >= $versionFromGithubJson->version) {
                             // throw new Exception('Current version [' . $currentVersion . '] is newer or equal to Github version [' . $versionFromGithubJson->version . ']');
                         }
 
                         //
-                        if ($versionFromGithub == '') {
-                            throw new Exception('Cannot retrieve version information from GitHub');
+                        print "<div class='status'>Download current version</div>";
+                        $zipUrl = 'https://codeload.github.com/HerrOtto/beyond/zip/master?nocache=' . urlencode(microtime(true) . bin2hex(random_bytes(10)));
+                        $zipFile = __DIR__ . '/temp/update.zip';
+                        //$zipResult = $tools->httpGet($zipUrl, $zipFile, 60);
+                        //if ($zipResult !== true) {
+                        //    throw new Exception('Cannot retrieve update from GitHub [https://codeload.github.com/HerrOtto/beyond/zip/master]');
+                        //}
+                        print '<div class="note">Download done</div>';
+
+                        // Extract ZIP archive
+                        print "<div class='status'>Extracting files!</div>";
+                        $error = false;
+                        $za = new ZipArchive();
+                        if ($za->open($zipFile) !== true) {
+                            throw new Exception('Error processing ZIP archive [' . $zipFile . ']');
+                        } else {
+                            for ($i = 0; $i < $za->numFiles; $i++) {
+                                $stat = $za->statIndex($i);
+
+                                if (preg_match('/^beyond\-master\/config\//', $stat['name'])) {
+                                    print '<div class="note">IGNORE: ' . $stat['name'] . '</div>';
+                                } else if (preg_match('/^beyond\-master(\/.*)/', $stat['name'], $matches)) {
+
+                                    // Extract
+                                    if ($data = $za->getFromName($stat['name'])) {
+                                        if (file_put_contents(__DIR__ . $matches[1], $data) === false) {
+                                            print '<div class="note">ERROR: ' . $stat['name'] . ' (' . $matches[1] . ')</div>';
+                                            $error = true;
+                                        } else {
+                                            print '<div class="note">UPDATE: ' . $stat['name'] . ' (' . $matches[1] . ')</div>';
+                                        }
+                                    }
+
+                                } else {
+                                    print '<div class="note">ERROR: ' . $stat['name'] . '</div>';
+                                }
+                            }
+                        }
+                        if ($error === true) {
+                            throw new Exception('Not all files were written successfully');
                         }
 
+                        print "<div class='status'>Done!</div>";
 
                     } catch (Exception $e) {
-                        print 'Exception: ' . $e->getMessage();
+                        print '<div class="error">' . $e->getMessage() . '</div>';
                     }
                     ?>
 
