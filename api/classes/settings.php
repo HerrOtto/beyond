775376311d54304dcffaf701dc0aeee490add3c2
Saxen <info@netzmal.de>
2021-03-12 08:04:50 +0100

    Cleanup

---
+++ api/classes/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once __DIR__ . '/../apiBaseClass.php';
+
+class settings extends apiBaseClass
+{
+
+    // TODO: List config files
+
+    // TODO: Load config file
+
+    // TODO: Save config file
+
+}
