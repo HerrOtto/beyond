e1c962d56156fcc986912e26d6534012ebdce32a
Saxen <info@netzmal.de>
2021-05-20 13:10:11 +0200

    Check captcha from PHP script

--- plugins/captcha/init.php
+++ plugins/captcha/init.php
@@ -2,7 +2,12 @@
 
 // Called from: ../../inc/init.php
 
-$beyond->plugins->captcha = new captchaHandler(
+$beyond->plugins->captcha = new stdClass();
+
+// Includes
+include_once __DIR__ . '/inc/class_captchaHandler.php';
+
+$beyond->captcha = new captchaHandler(
     $_SESSION[$beyond->prefix . 'data']['language'],
     $beyond->prefix,
     $beyond->tools,
